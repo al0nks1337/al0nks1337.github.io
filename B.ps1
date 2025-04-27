2e2605a2ce796ff5dcb7d1a45ad9a6667c594b54
al0nks1337 <al0nks1337@gmail.com>
2025-04-27 15:59:53 +0000

    Create B.ps1

---
+++ B.ps1
@@ -0,0 +1,8 @@
+Clear-Host
+Start-Process -FilePath "changepk.exe" -ArgumentList "/ProductKey NPPR9-FWDCX-D2C8J-H872K-2YT43" -NoNewWindow -Wait -RedirectStandardOutput $null -RedirectStandardError $null
+cscript.exe //nologo slmgr.vbs /ipk CPWHC-NT2C7-VYW78-DHDB2-PG3GK *> $null
+$temp = [System.IO.Path]::GetTempPath()
+$masPath = Join-Path -Path $temp -ChildPath "MAS_AIO.cmd"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" -OutFile $masPath
+Start-Process -FilePath $masPath -ArgumentList "/HWID" -Wait
+Remove-Item -Path $masPath -Force
