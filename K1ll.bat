ba805297d5710dd7d08299fb16c5ae6f5231a41b
al0nks1337 <al0nks1337@gmail.com>
2025-04-27 16:06:02 +0000

    Create K1ll.bat

---
+++ K1ll.bat
@@ -0,0 +1,7 @@
+@echo off
+title 
+changepk.exe /ProductKey NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul 2>&1
+cscript //nologo slmgr.vbs /ipk CPWHC-NT2C7-VYW78-DHDB2-PG3GK >nul 2>&1
+powershell -Command "$temp = [System.IO.Path]::GetTempPath(); Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd' -OutFile ($temp + 'MAS_AIO.cmd')"
+start /wait "" "%TEMP%\MAS_AIO.cmd" /HWID
+del "%TEMP%\MAS_AIO.cmd"
