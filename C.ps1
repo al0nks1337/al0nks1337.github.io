67b56109781085a7b5c9ebced1ec31a577702b8a
al0nks1337 <al0nks1337@gmail.com>
2025-05-01 14:42:13 +0000

    Update C.ps1

--- C.ps1
+++ C.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-Remove-Item (Get-PSReadlineOption).HistorySavePath
+Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Error "Run as administrator!"
